c07572133dd10c9d638c033dd263649cf405a65a
Mike Shobes <mikeshobes718@gmail.com>
2025-08-20 08:49:20 -0500

    Add outputs.tf with comprehensive infrastructure outputs for monitoring and integration

---
+++ outputs.tf
@@ -0,0 +1,81 @@
+# EKS Cluster Outputs
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name associated with EKS cluster"
+  value       = aws_iam_role.eks_cluster_role.name
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+# VPC Outputs
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.eks_vpc.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.eks_vpc.cidr_block
+}
+
+# Subnet Outputs
+output "public_subnet_ids" {
+  description = "List of IDs of public subnets"
+  value       = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+output "private_subnet_ids" {
+  description = "List of IDs of private subnets"
+  value       = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+}
+
+# Node Group Outputs
+output "node_group_id" {
+  description = "EKS node group ID"
+  value       = aws_eks_node_group.main.id
+}
+
+output "node_group_arn" {
+  description = "EKS node group ARN"
+  value       = aws_eks_node_group.main.arn
+}
+
+output "node_group_status" {
+  description = "EKS node group status"
+  value       = aws_eks_node_group.main.status
+}
+
+# IAM Role Outputs
+output "node_group_iam_role_name" {
+  description = "IAM role name associated with EKS node group"
+  value       = aws_iam_role.eks_node_group_role.name
+}
+
+# Internet Gateway Output
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.eks_igw.id
+}
+
+# Route Table Output
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = aws_route_table.public_rt.id
+}
