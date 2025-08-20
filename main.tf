4a1f3b0d4be37f802894091b55b652a33b149a34
Mike Shobes <mikeshobes718@gmail.com>
2025-08-20 10:03:07 -0500

    Fix: Change instance type to t3.small for Free Tier compatibility

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ resource "aws_eks_node_group" "main" {
     min_size     = 1
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node_policy,
